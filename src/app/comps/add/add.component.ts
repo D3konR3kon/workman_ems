fa53b8d48aedf5bbfa32a325508deea9b906bc53
D3konR3kon <dilavuyo@outlook.com>
2023-12-14 10:36:11 +0200

    hghghghgh

--- src/app/comps/add/add.component.ts
+++ src/app/comps/add/add.component.ts
@@ -8,31 +8,46 @@ import { FormGroup, FormControl, Validators, FormBuilder, EmailValidator } from 
 })
 export class AddComponent implements OnInit{
   
-  myForm =this.formBuilder.group({
-    fname: ['', Validators.minLength(2)],
-    lname: ['', Validators.minLength(2)],
-    sex: ['', Validators.minLength(2)],
-    email: ['', Validators.minLength(2)],
-    age: [0, Validators.minLength(2)],
-    status: [0, Validators.minLength(2)],
-    dept: ['', Validators.minLength(2)],
-    salary: [0, Validators.minLength(2)],
-    id_number: ['', Validators.minLength(2)],
-    position: ['', Validators.minLength(2)],
-    contract: ['', Validators.minLength(2)],
-    start_date: ['', Validators.minLength(2)],
-    cell_num: ['', Validators.minLength(2)],
-    address: ['', Validators.minLength(2)],
-    password: ['', Validators.minLength(5)],
-    
-  });
-  constructor(private formBuilder: FormBuilder) {}
+  submitted= false;
+  success = "none"
+  myForm:FormGroup
+  
+  constructor(private formBuilder: FormBuilder) {
+    this.myForm =this.formBuilder.group({
+      fname: ['',[Validators.required ,Validators.minLength(2)]],
+      lname: ['',[Validators.required, Validators.minLength(2)]],
+      sex: ['',[Validators.required, Validators.minLength(2)]],
+      email: ['',[Validators.required, Validators.minLength(2)]],
+      age: ['', [Validators.required, Validators.minLength(2)]],
+      status: ["", [Validators.required, Validators.minLength(2)]],
+      dept: ['',[Validators.required, Validators.minLength(2)]],
+      salary: ['', [Validators.required, Validators.minLength(2)]],
+      id_number: ['', [Validators.required, Validators.minLength(2)]],
+      position: ['',[Validators.required,  Validators.minLength(2)]],
+      contract: ['',[Validators.required,  Validators.minLength(2)]],
+      start_date: ['',[Validators.required, Validators.minLength(2)]],
+      cell_num: ['',[ Validators.required, Validators.minLength(2)]],
+      address: ['',[Validators.required, Validators.minLength(2)]],
+      password: ['',[Validators.required, Validators.minLength(5)]],
+      
+    });
+  }
   ngOnInit(): void {
-    this.myForm
+    
   }
   onSubmit() {
     // TODO: Use EventEmitter with form value
-    console.log(this.myForm.value);
+    
+    if(this.myForm.value.fname != ""){
+      console.log(this.myForm.valid)
+      this.submitted = true
+    }
+    setTimeout(()=>{
+      this.submitted = false
+    },4000)
+  }
+  clearinputs(){
+    this.myForm.reset()
   }
   
 }
